0719e1d5e990d21b7f84829194f12e7c85652cec
Sential <sentialx@gmail.com>
2018-03-27 20:50:26 +0200

    :ambulance: Remove redundant parameter from list function

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,18 +3,19 @@ import path from 'path';
 
 import { readPackage } from '../utils/packages';
 import Package from '../models/package'; // eslint-disable-line
+import { pluginsPath } from '../config';
 
-export default (packagesDir: string) =>
+export default () =>
   new Promise((resolve: (plugins: Package[]) => void, reject) => {
     const packages = [];
 
-    fs.readdir(packagesDir, (err, dirs) => {
+    fs.readdir(pluginsPath, (err, dirs) => {
       if (err) {
         reject(err);
       }
 
       for (const dir of dirs) {
-        const pkgDir = path.resolve(packagesDir, dir);
+        const pkgDir = path.resolve(pluginsPath, dir);
         packages.push(readPackage(pkgDir));
       }
 
